2316cc90b4f86e84d4adaec6ff2676e6a5e41080
shakil ahmed <shakiltha338@gmail.com>
2024-03-13 03:07:07 +0600

    added deletion functionality form bookmarked blogs

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,14 @@ function App() {
     setBookmarks(newBookmarks);
   };
 
-  const handleMarkAsRead = (time) => {
+  const handleMarkAsRead = (time, id) => {
     setReadingTime(readingTime + time);
+    // remove the read blog from bookmark
+    // console.log("remove bookmark", id);
+    const remainingBookmarks = bookmarks.filter(
+      (bookmark) => bookmark.id !== id
+    );
+    setBookmarks(remainingBookmarks);
   };
 
   return (

8af6e983b9376a14ef1f5ef5c32e3b7d90756ce5
shakil ahmed <shakiltha338@gmail.com>
2024-03-13 01:16:10 +0600

    added blog component and proptypes

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -10,8 +10,8 @@ const Blogs = () => {
       .then((data) => setBlogs(data));
   }, []);
   return (
-    <div>
-      <h2>Blogs</h2>
+    <div className="md:w-2/3">
+      <h2 className="text-4xl">Blogs: {blogs.length}</h2>
     </div>
   );
 };
